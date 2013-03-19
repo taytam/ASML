69f963bbeb2fff70f2d98d8dc6618bc6db9869de
Zackary <ZBreitenstein1@gmail.com>
2013-03-18 18:20:42 -0700

    Project 1 ASML GUI CODE
    Project 1 ASML GUI code includes the following:
    -Live video feed
    -Start button to start video feed
    -Stop button to stop video feed
    **Current known bugs: May take a few minutes for live video feed to
    speed up to real time. **
    -Read in Ini and Xml files to Target List box
    -Uses Configuration button and Load Target Info button
    -Configuration button
    -Opens a separate window that will allow the user to select a file from
    the file explorer window. Press "select" and then "open"
    **There are currently no default extensions to look for so unknown
    errors will occur when selecting target files of non .ini/.xml. **
    -After selecting the .ini/.xml file close Configuration window and then
    select the Load Target Info button to read in target coordinates
    **Currently no way of clearing target list and no way of avoiding having
    duplicate button presses of Load Target Info. The target information
    will get added every time the Load Target Info button is pressed. Each
    line of the Target list box is selectable might need to look into
    possibly disabling selecting list items. **
    -Up/Down/Left/Right/Fire buttons all work with missile launcher
    **Fixed the on button hold problem. Missile launcher will continue to
    move when button is held down. **
    -Displays Team name
    -Displays updated version number
    **Will need to update this number starting from this version is any
    minor changes are made use the format #.# (e.g. Version 1.1 meaning
    release version 1 minor change 1) If any major changes are made such as
    search and destroy use the format #.# (e.g. Version 2.0 meaning release
    version 2 with no minor changes yet made). **
    -# of Missiles/Timer label is currently set to 0 with no functionality
    -Select Video Device will allow the user to select which video device
    the program will use.
    **Have not tested with multiple cameras unknown errors may occur. **

---
+++ N7_GUI_Project1/N7_GUI_Project1/Configuration.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace N7_GUI_Project1
+{
+    partial class Configuration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.File_Text = new System.Windows.Forms.TextBox();
+            this.File_Select = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // File_Text
+            // 
+            this.File_Text.Location = new System.Drawing.Point(65, 16);
+            this.File_Text.Name = "File_Text";
+            this.File_Text.Size = new System.Drawing.Size(193, 20);
+            this.File_Text.TabIndex = 1;
+            // 
+            // File_Select
+            // 
+            this.File_Select.Location = new System.Drawing.Point(64, 47);
+            this.File_Select.Name = "File_Select";
+            this.File_Select.Size = new System.Drawing.Size(78, 27);
+            this.File_Select.TabIndex = 2;
+            this.File_Select.Text = "Select File";
+            this.File_Select.UseVisualStyleBackColor = true;
+            this.File_Select.Click += new System.EventHandler(this.Select_File);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File:";
+            // 
+            // Configuration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.File_Select);
+            this.Controls.Add(this.File_Text);
+            this.Name = "Configuration";
+            this.Text = "Configuration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox File_Text;
+        private System.Windows.Forms.Button File_Select;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ N7_GUI_Project1/N7_GUI_Project1/MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Data;
+using Microsoft.Expression.Encoder.Devices;
+using WebcamControl;
+using UsbLibrary;
+
+namespace N7_GUI_Project1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+     
+    public partial class MainWindow : Window
+    {
+        /**Creates a MissileLauncher object for the MainWindow class**/
+
+        static MissileLauncher launcher = new MissileLauncher();
+      
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            /**Bind the Video device properties of the
+             * Webcam control to the SelectedValue property of 
+             * the necessary ComboBox**/
+            Binding bndg_1 = new Binding("SelectedValue");
+            bndg_1.Source = VidDvcsComboBox;
+            WebCamCtrl.SetBinding(Webcam.VideoDeviceProperty, bndg_1);
+
+            /**Create directory for saving video files**/
+            string vidPath = @"C:\VideoClips";
+
+            if (Directory.Exists(vidPath) == false)
+            {
+                Directory.CreateDirectory(vidPath);
+            }
+
+            /**Create directory for saving image files**/
+            string imgPath = @"C:\WebcamSnapshots";
+
+            if (Directory.Exists(imgPath) == false)
+            {
+                Directory.CreateDirectory(imgPath);
+            }
+
+            /**Set the Webcam video feed properties**/
+            WebCamCtrl.VideoDirectory = vidPath;
+            WebCamCtrl.VidFormat = VideoFormat.mp4;
+
+            /**Set the Webcam image properties**/
+            WebCamCtrl.ImageDirectory = imgPath;
+            WebCamCtrl.PictureFormat = ImageFormat.Jpeg;
+
+            WebCamCtrl.FrameRate = 30;
+            WebCamCtrl.FrameSize = new System.Drawing.Size(320, 240);
+
+            /**Find a/v devices connected to the machine**/
+            FindDevices();
+
+            VidDvcsComboBox.SelectedIndex = 0;
+        }
+
+        /**FindDevices looks for all possible webcam devices available on the
+         * computer**/
+        public void FindDevices()
+        {
+            var vidDevices = EncoderDevices.FindDevices(EncoderDeviceType.Video);
+
+            foreach (EncoderDevice dvc in vidDevices)
+            {
+                VidDvcsComboBox.Items.Add(dvc.Name);
+            }
+        }
+
+        /**Start_Button_Click start the live video feed**/
+        public void Start_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                /**Display webcam video on control**/
+                WebCamCtrl.StartCapture();
+
+                launcher.command_reset();
+            }
+            catch (Microsoft.Expression.Encoder.SystemErrorException ex)
+            {
+                MessageBox.Show("Device is in use by another application");
+            }
+        }
+
+        /**Stop_Button_Click stops the live video feed**/
+        public void Stop_Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Stop the display of webcam video.
+            WebCamCtrl.StopCapture();
+        }
+
+        /**public void RecordButton_Click(object sender, RoutedEventArgs e)
+        {
+             Start recording of webcam video to harddisk.
+        }
+
+        public void StopRecordButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Stop recording of webcam video to harddisk.
+            WebCamCtrl.StopRecording();
+        }
+
+        public void SnapshotButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Take snapshot of webcam image.
+            WebCamCtrl.TakeSnapshot();
+        }**/
+
+        /**Up Button press to move turret up**/
+        public void move_up(object sender, RoutedEventArgs e)
+        {
+            launcher.command_Up(25);
+        }
+
+        /**Down Button press to move turret down**/
+        public void move_down(object sender, RoutedEventArgs e)
+        {
+            launcher.command_Down(25);
+        }
+
+        /**Left Button press to move the turret left**/
+        public void move_left(object sender, RoutedEventArgs e)
+        {
+           launcher.command_Left(25);
+        }
+
+        /**Right Button press to move the turret right**/
+        public void move_right(object sender, RoutedEventArgs e)
+        {
+            launcher.command_Right(25);
+        }
+
+        /**Function fires the turret**/
+        public void Fire(object sender, RoutedEventArgs e)
+        {
+            launcher.command_Fire();
+        }
+
+        /**Show_Configuration_Window brings up the file 
+         * reader configuration window**/
+        public void Show_Configuration_Window(object sender, RoutedEventArgs e)
+        {
+            
+        /**Creates a Configuration window object to that the user can
+         * give the program the ini or xml file**/
+            Configuration configuration_Window = new Configuration();
+            configuration_Window.Show();
+        }
+
+        /**Load_Target_Info takes target coordinates that are read from the
+         * ini/xml file that is loaded in by the user**/
+        public void Load_Target_Info(object sender, RoutedEventArgs e)
+        {
+            string value = System.Configuration.ConfigurationManager.AppSettings["Target_File"];
+
+            FileReader file;
+            FilereaderFactory fileFactory = new FilereaderFactory();
+            file = fileFactory.Check_File_Type(value);
+
+            /**If the file has an invalid format handler**/
+            if (file == null)
+            {
+                Environment.Exit(-1);
+            }
+
+            List<string> outputFile = file.Read_File(value);
+
+            Write_Target_Info(outputFile);
+        }
+
+        /**Write_Target_Info writes out a list of strings to the ListBox
+         * item containing the target information**/
+        public void Write_Target_Info(List<string> list)
+        {
+            foreach (string item in list)
+                targetInformation.Items.Add(item); 
+        }
+    }
+}
