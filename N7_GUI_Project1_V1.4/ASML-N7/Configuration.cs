625b8f482d785e1805ecaa0a221a4d0fd9e50594
Zackary <ZBreitenstein1@gmail.com>
2013-03-19 21:54:34 -0700

    N7 GUI Project V1.4
    Project 1 ASML GUI code V1.4 includes the following:
    -Live video feed
    -Start button to start video feed
    -Stop button to stop video feed
    **Current known bugs: May take a few minutes for live video feed to
    speed up to real time. **
    -Read in Ini and Xml files to Target List box
    -ListBox items are cleared for every new Ini/Xml file read in
    -Uses Configuration button and Load Target Info button
    -Configuration button
    -Opens a separate window that will allow the user to select a file from
    the file explorer window. Press "select" and then "open"
    **Only Allows for files of .ini extension**
    -After selecting the .ini file close Configuration window and then
    select the Load Target Info button to read in target coordinates
    -Up/Down/Left/Right/Fire buttons all work with missile launcher
    -Displays Team name
    -Displays updated version number
    **Will need to update this number starting from this version is any
    minor changes are made use the format #.# (e.g. Version 1.1 meaning
    release version 1 minor change 1) If any major changes are made such as
    search and destroy use the format #.# (e.g. Version 2.0 meaning release
    version 2 with no minor changes yet made). **
    -# of Missiles/Timer label is currently set to 0 with no functionality
    -Select Video Device will allow the user to select which video device
    the program will use.
    **Have not tested with multiple cameras unknown errors may occur. **

---
+++ N7_GUI_Project1_V1.4/ASML-N7/Configuration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace ASML_N7
+{
+    public partial class Configuration : Form
+    {
+        /**Default constructor for the Configuration window**/
+        public Configuration()
+        {
+            InitializeComponent();
+            Load_Configuration();
+        }
+
+        /**Load the most recent App.Config target file path**/
+        private void Load_Configuration()
+        {
+            /**Load the target path into the Label File_Text**/
+            File_Text.Text = ConfigurationManager.AppSettings["Target_File"];
+        }
+
+        /**Save the selected file path to the App.Config file**/
+        private void Save_Configuration(string filename)
+        {
+            /**ConfigurationManager object to configure App.Config settings**/
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            /**Find the AppSetting Key "Target_File" and set the value to the filename
+             * of the ini/xml file input by the user**/
+            config.AppSettings.Settings["Target_File"].Value = filename;
+
+            /**Save the App.Config settings**/
+            config.Save(ConfigurationSaveMode.Modified);
+
+            /**Refesh the changes made to the App.Config file**/
+            ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+        }
+
+        private void Select_File(object sender, EventArgs e)
+        {
+            /**Create OpenFileDialog**/ 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            /**Set filter for file extension and default file extension**/
+            dlg.DefaultExt = ".txt";
+            dlg.DefaultExt = ".ini";
+            dlg.Filter = "Text Documents (.txt)|*.txt";
+            dlg.Filter = "Ini File (.ini)|*.ini";
+
+            /**Display OpenFileDialog by calling ShowDialog method**/ 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            /**Get the selected file name and display in a TextBox**/
+            if (result == true)
+            {
+                /**Open document**/ 
+                string filename = dlg.FileName;
+                File_Text.Text = filename;
+                Save_Configuration(filename);
+            }
+        }
+    }
+}
---
+++ N7_GUI_Project1_V1.4/ASML-N7/Target.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASML_N7
+{
+    public class Target
+    {
+        /**xPos, yPos, zPos are used for identifying target coordinates**/
+        private int xPos;
+        private int yPos;
+        private int zPos;
+
+        /**String name will be used to hold the name value of targets read in by
+         * the Ini/Xml target files**/
+        private string name;
+
+        /**_isFriend is used to identify if the target is a freind or foe**/
+        private bool _isFriend;
+
+        /**_isEliminated is used to identify if the target has been successfully
+         * eliminated(Hit)**/
+        private bool _isEliminated;
+
+        /**Get/Set for int xPos**/
+        public int XPosition
+        {
+            get { return xPos; }
+            set { xPos = value; }
+        }
+
+        /**Get/Set for int yPos**/
+        public int YPosition
+        {
+            get { return yPos; }
+            set { yPos = value; }
+        }
+
+        /**Get/Set for int zPos**/
+        public int ZPosition
+        {
+            get { return zPos; }
+            set { zPos = value; }
+        }
+
+        /**Get/Set for string name**/
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /**Get/Set for bool _isFriend**/
+        public bool isFriend
+        {
+            get { return _isFriend; }
+            set { _isFriend = value; }
+        }
+
+        /**Get/Set for bool _isEliminated**/
+        public bool isEliminated
+        {
+            get { return _isEliminated; }
+            set { _isEliminated = value; }
+        }
+
+        /**Default constructor for the Target class all x,y,z positions are
+         * initialized to 0 and name is set to null. All targets before being
+         * identified are labeled as unfriendly and not eliminated**/
+        public Target()
+        {
+            xPos = 0;
+            yPos = 0;
+            zPos = 0;
+
+            name = null;
+
+            _isFriend = false;
+            _isEliminated = false;
+        }
+
+        public Target(int xPosition, int yPosition, int zPosition, string targetName, bool isFriend, bool isEliminated)
+        {
+            xPos = xPosition;
+            yPos = yPosition;
+            zPos = zPosition;
+
+            name = targetName;
+
+            _isFriend = isFriend;
+            _isEliminated = isEliminated;
+        }
+    }
+}
